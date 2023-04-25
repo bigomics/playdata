c4026e082eb3acd9377907e5600bc7cfb8dc39d9
ncullen93 <nicholas.cullen@med.lu.se>
2023-04-25 13:49:59 +0200

    add gmt files to inst/extdata and add new get_file() function for accessing them

---
+++ R/get_file.R
@@ -0,0 +1,21 @@
+
+#' Get path to playdata file in inst/extdata
+#'
+#' `playdata` comes bundled with a number of non-RDA files in its `inst/extdata`
+#' directory. This function make them easy to access. This function was
+#' taken from tidyverse/readr.
+#'
+#' @param file string. Name of file. If `NULL`, the example files will
+#'   be listed.
+
+#' @examples
+#' get_fiel()
+#' get_file("kinase_substrates_kea.gmt")
+#' @export
+get_file <- function(file = NULL) {
+    if (is.null(file)) {
+        dir(system.file("extdata", package = "playdata"))
+    } else {
+        system.file("extdata", file, package = "playdata", mustWork = TRUE)
+    }
+}
