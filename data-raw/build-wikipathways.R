69cbee7688e2540e493296928bb865a99c2b0ff8
Mauro Masiero <mauromiguelm@hotmail.com>
2023-05-09 17:24:36 +0200

    remove filter from getBM function (not working)

--- data-raw/build-wikipathways.R
+++ data-raw/build-wikipathways.R
@@ -16,7 +16,6 @@ ensembl = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 
 # Read wikipathways GMT file
 
-
 # eventually we can download wikipathways from different species with 
 # https://bioconductor.org/packages/release/bioc/html/rWikiPathways.html
 gmt <- readGMT('data-raw/extdata/wikipathways-20230410-gmt-Homo_sapiens.gmt')
@@ -24,10 +23,12 @@ gmt <- readGMT('data-raw/extdata/wikipathways-20230410-gmt-Homo_sapiens.gmt')
 # Retrieve the gene symbols using the getBM function
 
 gene_symbols = getBM(attributes = c("entrezgene_id", "hgnc_symbol"),
-                     filters = "entrezgene_id",
-                     values = gmt[2],
+                    # filters = "entrezgene_id",
+                    # values = as.numeric(gmt$gene),
                      mart = ensembl)
-gmt$gene <- gene_symbols[match(gmt[,2], gene_symbols[,1]),2]
+
+gmt$gene <- gene_symbols[match(gmt$gene, gene_symbols$entrezgene_id),2]
+
+gmt <- gmt[!is.na(gmt$gene),]
 
 writeGMT(gmt, "data-raw/extdata/gmt/Pathway_wikipathways_hsa_2023.gmt")
-
