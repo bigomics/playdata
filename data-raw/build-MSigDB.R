610eabe350e2784dd14fb2b1e0f15a4a1387411d
Mauro Masiero <mauromiguelm@hotmail.com>
2023-05-10 11:21:10 +0200

    remove url as first gene and add gs metadata

--- data-raw/build-MSigDB.R
+++ data-raw/build-MSigDB.R
@@ -16,19 +16,7 @@ gmt <-  playbase::convert.gmt(
     all_gene_sets$gene_symbol,
     gs_name = all_gene_sets$gs_name)
 
-# add url as first gene in case its necessary
-# gmt_2 <- lapply(names(gmt), function(x){
-#     genes <- gmt[[x]]
-#     url <- unique(url[[x]])
-#     if(url ==""){
-#         return(genes)
-#     }
-#     return(c(url, genes))
-# })
-# url <- playbase::convert.gmt(
-#     all_gene_sets$gs_url,
-#     gs_name = all_gene_sets$gs_name)
-# names(gmt_2) <- names(gmt)
-# gmt_2["ADA2_TARGET_GENES"]
-
 playbase::write.gmt(gmt, file= file.path("data-raw","extdata","gmt_msigdb","all_gene_sets.txt"))
+
+meta <- all_gene_sets[!duplicated(all_gene_sets[ , c("gs_name")]),]
+write.csv(meta ,file.path("data-raw","extdata","gmt_msigdb","meta.csv"))
