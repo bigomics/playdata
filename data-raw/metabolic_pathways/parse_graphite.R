13a5c0d5e5d95e61d91e7e830986892cb09b1773
Ivo Kwee <ivo.kwee@gmail.com>
2025-01-10 13:11:40 +0100

    update metabolomics annotation

--- data-raw/metabolic_pathways/parse_graphite.R
+++ data-raw/metabolic_pathways/parse_graphite.R
@@ -6,90 +6,107 @@
 library(graphite)
 library(igraph)
 
-  
-if(0) {
-
-  pathwayDatabases()
-  pathwayDatabase()
-  
-  pathways <- graphite::pathways("hsapiens", "reactome")
-  pathways <- graphite::pathways("hsapiens", "pathbank")
-  pathways <- graphite::pathways("hsapiens", "smpdb")
-  pathways <- graphite::pathways("hsapiens", "pharmgkb")
-  length(pathways)
-  names(pathways)[1:10]
-  
-  p <- pathways[[2]]
-  p
-  pathwayTitle(p)
-  nodes(p)
-  nodes(p, which = "mixed")
-  edges(p)
-  nodes(p, which = "mixed")
-  edges(p, which = "mixed")
-  head(edges(p, which = "metabolites"))
-    
-  g <- pathwayGraph(p, which = "mixed")
-  print(g)
-  g <- graph_from_graphnel(g)
-  plot(g, vertex.label.cex=1, vertex.size=1)
-  
-}
-
 pathwayDatabases()
 
-## Collect all edges from all databases
-all.db <- pathwayDatabases()[pathwayDatabases()$species=="mmusculus",2]
-all.db <- pathwayDatabases()[pathwayDatabases()$species=="hsapiens",2]
-all.db
-
-all.db <- c("panther","pathbank","reactome","wikipathways")
-db="reactome"
+## Collect all edges from all databases: kegg, panther, pathbank,
+## pharmgkb, reactome, smpdb, wikipathways
+##dbs <- graphite:::.dbs[['hsapiens']]
+dbs <- pathwayDatabases()[pathwayDatabases()$species=="hsapiens",2]
+dbs
+all.db <- setdiff(dbs, "smpdb")
+pathways <- list()
 db="kegg"
-all.edges <- c()
-
 for(db in all.db) {
-  pathways <- graphite::pathways("hsapiens", db)
-  length(pathways)
-  ##p1 = pathways[[k]]
-  message("****** ",db," *******")
-  
-  getEE <- function(p1) {
-    p1 <- convertIdentifiers(p1, "CHEBI")
-    p1 <- convertIdentifiers(p1, "SYMBOL")
-    ee <- edges(p1, which = "mixed")
-    g <- pathwayGraph(p1, which = "mixed")
-    g1 <- graph_from_graphnel(g)
-    pw.id <- paste0(p1@database,":",p1@id)
-    ee1 <- cbind( igraph::get.edgelist(g1), pw.id )
-    ee1
-  }
-
-  message("length.pathways = ", length(pathways))
-  ee <- parallel::mclapply( pathways, getEE, mc.cores = 48)
-  ee <- do.call( rbind, ee )
-  all.edges <- rbind(all.edges, ee)
-
-  message("nrow.ee = ", nrow(all.edges))
-  message("ncol.ee = ", ncol(all.edges))
-  
-  ## collapse multiple, count edges
-  gr <- igraph::graph_from_edgelist(all.edges[,1:2], directed=FALSE)
-  E(gr)$weight <- count_multiple(gr)
-  sum(E(gr)$weight[!which_multiple(gr)])
-  
-  gr2 <- simplify(gr, edge.attr.comb = list(weight = "min"))
-  ee <- igraph::get.edgelist(gr2) 
-  ee <- apply(ee, 2, function(a) sub("SYMBOL:","",a))
-  ee <- as.data.frame(ee) 
-  ee$cost <- 1 / E(gr2)$weight
-  GRAPHITE_PPI <- data.frame(from = ee[,1], to = ee[,2], cost = ee$cost)
-  head(GRAPHITE_PPI)
-
-  ## save(GRAPHITE_PPI, file="GRAPHITE_PPI.rda")
-  ## load("~/Playground/public-db/pathbank.org/GRAPHITE_PPI.rda",verbose=TRUE)
-
-  usethis::use_data(GRAPHITE_PPI, overwrite = TRUE)
-  
-  
+  paths <- graphite::pathways("hsapiens", db)
+    pathways[[db]] <- paths
 }
+save(pathways, file="graphite-pathways.rda")
+
+## get all nodes to create gene sets. Get all edges for creating PPI.
+load(file="graphite-pathways-symbol-chebi.rda",verbose=1)
+names(pathways)
+pathways[[1]][[1]]
+all.nodes <- list()
+all.edges <- list()
+for(i in names(pathways)) {
+  nn <- lapply(pathways[[i]], function(p) graphite::nodes(p, which="mixed"))
+  ee <- lapply(pathways[[i]], function(p) {
+    e1 <- graphite::edges(p,which="mixed")
+    cbind( paste0(e1$src_type,":",e1$src), paste0(e1$dest_type,":",e1$dest) )
+  })  
+  title <- sapply(pathways[[i]], function(p) p@title)
+  id <- sapply(pathways[[i]], function(p) p@id)
+  db <- sapply(pathways[[i]], function(p) p@database)    
+  tt <- paste0(title, " [",id,"]")
+  head(tt)
+  names(nn) <- tt
+  names(ee) <- tt
+  all.nodes <- c(all.nodes, nn)
+  all.edges <- c(all.edges, ee)
+}
+save(all.nodes, file="graphite-nodes.rda")
+save(all.edges, file="graphite-edges.rda")
+
+load(file="graphite-nodes.rda", verbose=1)
+load(file="graphite-edges.rda", verbose=1)
+
+#------------------------------------------------------------------------
+# Create pathways gene sets
+#------------------------------------------------------------------------
+
+gmt1 <- list(CUSTOM = all.nodes)
+pathways <- playbase::clean_gmt(gmt1, "PATHWAY_GRAPHITE")
+head(names(pathways))
+
+##pwsize <- sapply(pathways, length)
+msize <- sapply(pathways, function(s) sum(grepl("CHEBI",s)))
+pathways <- pathways[order(-msize)]
+pwname <- gsub(" \\[.*","",names(pathways))
+table(duplicated(pwname))
+pathways <- pathways[!duplicated(pwname)]
+
+wsize <- sapply(pathways, length)
+msize <- sapply(pathways, function(s) sum(grepl("CHEBI",s)))
+psize <- sapply(pathways, function(s) sum(grepl("SYMBOL",s)))
+table( msize >= 3)
+pathways <- pathways[which(msize >=3)]
+length(pathways)
+
+MSETxMETABOLITE <- playbase::createSparseGenesetMatrix(
+    pathways,
+    filter_genes = FALSE,
+    min.geneset.size = 3,
+    max.geneset.size = 500
+)
+
+dim(MSETxMETABOLITE)
+dim(playdata::MSETxMETABOLITE)
+head(rownames(MSETxMETABOLITE))
+head(colnames(MSETxMETABOLITE))
+tail(colnames(MSETxMETABOLITE))
+usethis::use_data(MSETxMETABOLITE, overwrite = TRUE)
+
+head(colnames(playdata::MSETxMETABOLITE))
+tail(colnames(playdata::MSETxMETABOLITE))
+
+##------------------------------------------------------------------------
+## Create PPI edgelist. collapse multiple, count edges
+##------------------------------------------------------------------------
+library(igraph)
+ee <- do.call(rbind, all.edges)
+sum(duplicated(ee))
+gr <- igraph::graph_from_edgelist(ee, directed=FALSE)
+E(gr)$weight <- count_multiple(gr)
+sum(E(gr)$weight[!which_multiple(gr)])
+
+gr2 <- simplify(gr, edge.attr.comb = list(weight = "min"))
+ee <- igraph::as_edgelist(gr2) 
+ee <- apply(ee, 2, function(a) sub("SYMBOL:","",a))
+ee <- as.data.frame(ee) 
+ee$cost <- 1 / E(gr2)$weight
+GRAPHITE_PPI <- data.frame(from = ee[,1], to = ee[,2], cost = ee$cost)
+head(GRAPHITE_PPI)
+
+## save(GRAPHITE_PPI, file="GRAPHITE_PPI.rda")
+## load("~/Playground/public-db/pathbank.org/GRAPHITE_PPI.rda",verbose=TRUE)
+usethis::use_data(GRAPHITE_PPI, overwrite = TRUE)
